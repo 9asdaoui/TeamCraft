3815f795abc6d0438588ac3a1fed8a7732bf97a1
9asdaoui <oussamaqasdaoui@gmail.com>
2024-11-28 16:16:31 +0100

    ft(js):fix errors in add/edit/delet functions)

--- script.js
+++ script.js
@@ -1,172 +1,198 @@
-
-
-let addBtn = document.getElementById("addbtn");
-let canBtn = document.getElementById("cancel");
-
 let players = [];
-let player = {};
-
-let localPlayers = JSON.parse(localStorage.getItem("players"));
-console.log(localPlayers);
-// localStorage.removeItem("players");
-if(localPlayers){players = localPlayers;}
 
 
-canBtn.addEventListener("click", function() {
-
-    let addPlayer = document.getElementById("addPlayer");
-addPlayer.setAttribute("style","display: none;");
-
-let body = document.getElementById("body");
-body.setAttribute("style","background-color: #f5f5f5");
-
-let field = document.getElementById("field");
-field.setAttribute("style","background-color: #2e8b57");
-
-});
-
-addBtn.addEventListener("click", function () {
-
-
-    let i = JSON.parse(localStorage.getItem("correntCard"));
-
-    let player = {
-        id: i,
-        imag:document.getElementById("playerPhoto").value,
-        name:document.getElementById("playerName").value,
-        club:document.getElementById("clubLogo").value,
-        position:document.getElementById("playerPosition").value,
-        rating:document.getElementById("playerRating").value,
-        flag:document.getElementById("flag").value,
-        pace:document.getElementById("playerPace").value,
-        shooting:document.getElementById("playerShooting").value,
-        passing:document.getElementById("playerPassing").value,
-        dribbling:document.getElementById("playerDribbling").value,
-        defending:document.getElementById("playerDefending").value,
-        physical:document.getElementById("playerPhysical").value,
-       
-    };
-console.log(player)
-    players.push(player);
-    localStorage.setItem("players", JSON.stringify(players));
-
-    document.getElementById("playerForm").reset();
-    affichage()
-      
-});
-
-
-
-function edit_Add (i){
-
-    let savedPlayers = JSON.parse(localStorage.getItem("players"));
-    let player = savedPlayers[i];
-
-    document.getElementById("playerPhoto").value = player?.imag ?? "";
-    document.getElementById("playerName").value = player?.name ?? "";
-    document.getElementById("clubLogo").value = player?.club ?? "";
-    document.getElementById("playerPosition").value = player?.position ?? "";
-    document.getElementById("playerRating").value = player?.rating ?? "";
-    document.getElementById("flag").value = player?.flag ?? "";
-    document.getElementById("playerPace").value = player?.pace ?? "";
-    document.getElementById("playerShooting").value = player?.shooting ?? "";
-    document.getElementById("playerPassing").value = player?.passing ?? "";
-    document.getElementById("playerDribbling").value = player?.dribbling ?? "";
-    document.getElementById("playerDefending").value = player?.defending ?? "";
-    document.getElementById("playerPhysical").value = player?.physical ?? "";
-
-
-let correntCard = i;
-localStorage.setItem("correntCard", JSON.stringify(correntCard));
-
-
-let addPlayer = document.getElementById("addPlayer");
-addPlayer.setAttribute("style","display: block;");
-}
-
-function delet(){
-
-}
-
-affichage()
-function affichage() {
-    let savedPlayers = JSON.parse(localStorage.getItem("players"));
-
-    if (!savedPlayers || savedPlayers.length === 0) {
-        console.error("No players found in localStorage.");
+function displays(){
+    let bar = document.getElementById("addPlayer")
+    bar.setAttribute("style","display: block;")
+ }
+function addCart() {
+    if (players.length >= 11) {
+        alert("No more players can be added.");
         return;
     }
 
-    for (let i = 0; i < 9 ; i++) {
-        let player = savedPlayers[i];
-        let j = player.id;
-        let divCard = document.getElementById(`card${j}`);
+    let inputs = document.querySelectorAll("#playerForm input");
 
-        if (!divCard) {
-            console.warn(`Card with id "card${j}" not found.`);
+    for (let input of inputs) {
+        if (!input.value.trim()) {
+            alert("Please fill all the required fields.");
+            return; 
         }
-
-        divCard.innerHTML = "";
-
-        let playerName = document.createElement("h3");
-        playerName.setAttribute("id","playerNamecard")
-        playerName.className = "player-name";
-        playerName.textContent = player ? player.name : ""; 
-        divCard.appendChild(playerName);
-
-        let img = document.createElement("img");
-        img.setAttribute("id","playerPhotocard")
-        img.src = player?.imag ?? "images/default-photo.png"; 
-        img.className = "player-photo";
-        divCard.appendChild(img);
-
-
-        if(player?.club){
-        let clubLogo = document.createElement("img");
-        clubLogo.setAttribute("id","clubLogocard")
-        clubLogo.src = player?.club ?? "";
-        clubLogo.className = "club-logo";
-        divCard.appendChild(clubLogo);}
-
-
-        if(player?.flag){
-        let flag = document.createElement("img");
-        flag.setAttribute("id","flagcard")
-        flag.src = player?.flag ?? "";
-        flag.className = "flag";
-        divCard.appendChild(flag);}
-
-        if(player?.flag){
-        let position = document.createElement("p");
-        position.setAttribute("id","playerPositioncard")
-        position.textContent = `${player?.position ?? ""}`;
-        divCard.appendChild(position);}
-
-        let rating = document.createElement("h5");      
-        rating.setAttribute("id","playerRatingcard")
-        rating.textContent = player ? `RATING: ${player.rating } ` : "";
-        divCard.appendChild(rating);
-
-       
-if(player?.pace){
-        let stats = document.createElement("ul");
-        let statsData = [
-            { stat: "PAC", value: player?.pace ?? ""},
-            { stat: "PAS", value: player?.passing ?? ""},
-            { stat: "SHO", value: player?.shooting ?? ""},
-            { stat: "PHY", value: player?.physical ?? ""},
-            { stat: "DRI", value: player?.dribbling ?? ""},
-            { stat: "DEF", value: player?.defending ?? ""},
-        ];
-
-        statsData.forEach(item => {
-            let li = document.createElement("li");
-            li.setAttribute("id",`${item.stat}`)
-            li.innerHTML = `<strong>${item.stat}:</strong> ${item.value}`;
-            stats.appendChild(li);
-        });
-    
-        divCard.appendChild(stats);}
     }
+    let player = {
+        id: players.length, 
+        image: document.getElementById("playerPhoto").value,
+        name: document.getElementById("playerName").value,
+        club: document.getElementById("clubLogo").value,
+        position: document.getElementById("playerPosition").value,
+        rating: document.getElementById("playerRating").value,
+        flag: document.getElementById("flag").value,
+        pace: document.getElementById("playerPace").value,
+        shooting: document.getElementById("playerShooting").value,
+        passing: document.getElementById("playerPassing").value,
+        dribbling: document.getElementById("playerDribbling").value,
+        defending: document.getElementById("playerDefending").value,
+        physical: document.getElementById("playerPhysical").value,
+    };
+
+    players.push(player);
+    document.getElementById("playerForm").reset();
+    affichage();
+    let bar = document.getElementById("addPlayer")
+    bar.setAttribute("style","display: none;")
 }
+
+function edit_Add(index) {
+    let bar = document.getElementById("addPlayer")
+    bar.setAttribute("style","display: block;")
+    let player = players[index];
+
+    document.getElementById("playerPhoto").value = player.image;
+    document.getElementById("playerName").value = player.name;
+    document.getElementById("clubLogo").value = player.club;
+    document.getElementById("playerPosition").value = player.position;
+    document.getElementById("playerRating").value = player.rating;
+    document.getElementById("flag").value = player.flag;
+    document.getElementById("playerPace").value = player.pace;
+    document.getElementById("playerShooting").value = player.shooting;
+    document.getElementById("playerPassing").value = player.passing;
+    document.getElementById("playerDribbling").value = player.dribbling;
+    document.getElementById("playerDefending").value = player.defending;
+    document.getElementById("playerPhysical").value = player.physical;
+
+    let btn = document.getElementById("addbtn");
+    btn.textContent = "Edit Player";
+    btn.setAttribute("onclick", `edit(${index})`);
+}
+
+function edit(index) {
+    let player = players[index];
+
+    player.image = document.getElementById("playerPhoto").value;
+    player.name = document.getElementById("playerName").value;
+    player.club = document.getElementById("clubLogo").value;
+    player.position = document.getElementById("playerPosition").value;
+    player.rating = document.getElementById("playerRating").value;
+    player.flag = document.getElementById("flag").value;
+    player.pace = document.getElementById("playerPace").value;
+    player.shooting = document.getElementById("playerShooting").value;
+    player.passing = document.getElementById("playerPassing").value;
+    player.dribbling = document.getElementById("playerDribbling").value;
+    player.defending = document.getElementById("playerDefending").value;
+    player.physical = document.getElementById("playerPhysical").value;
+
+    let btn = document.getElementById("addbtn");
+    btn.textContent = "Add Player";
+    btn.setAttribute("onclick", "addCart()");
+
+    document.getElementById("playerForm").reset();
+    affichage();
+    let bar = document.getElementById("addPlayer")
+    bar.setAttribute("style","display: none;")
+}
+
+function deletePlayer(index) {
+    players.splice(index, 1);
+    affichage();
+}
+
+function affichage() {
+    let rowForwards = document.getElementById("rowForwards");
+    let rowMidfielders = document.getElementById("rowMidfielders");
+    let rowDefenders = document.getElementById("rowDefenders");
+    let rowGoalkeeper = document.getElementById("rowGoalkeeper");
+
+    rowForwards.innerHTML = "";
+    rowMidfielders.innerHTML = "";
+    rowDefenders.innerHTML = "";
+
+    players.forEach((player, index) => {
+        let divCard = document.createElement("div");
+        divCard.classList.add("card");
+        divCard.setAttribute("id", `card${player.id}`);
+
+
+        divCard.innerHTML = `
+            <button type="button" class="action-btn" id="deletbtn" onclick="deletePlayer(${index})">X</button>
+            <button type="button" class="action-btn" id="editbtn" onclick="edit_Add(${index})">✎</button>
+
+            <h3 class="player-name">${player.name}</h3>
+            <img src="${player.image}" class="player-photo" alt="${player.name}">
+            <img src="${player.club}" class="club-logo" alt="Club">
+            <img src="${player.flag}" class="flag" alt="Flag">
+            <p class="position">${player.position}</p>
+            <h5 class="rating">Rating: ${player.rating}</h5>
+            <ul class="stats">
+                <li>Pace: ${player.pace}</li>
+                <li>Shooting: ${player.shooting}</li>
+                <li>Passing: ${player.passing}</li>
+                <li>Dribbling: ${player.dribbling}</li>
+                <li>Defending: ${player.defending}</li>
+                <li>Physical: ${player.physical}</li>
+            </ul>
+        `;
+
+        switch (player.position) {
+            case "CB":
+            case "LB":
+            case "RB":
+                rowDefenders.appendChild(divCard);
+                break;
+            case "CM":
+            case "LW":
+            case "RW":
+                rowMidfielders.appendChild(divCard);
+                break;
+            case "ST":
+                rowForwards.appendChild(divCard);
+                break;
+
+        }
+    });
+}
+
+
+// const apiUrl = 'https://cors-anywhere.herokuapp.com/https://drop-api.ea.com/rating/fc-24?limit=9';
+// let playersData = [];
+
+// fetch(apiUrl)
+//   .then(response => response.json())
+//   .then(data => {
+//     playersData = data;
+//     console.log(playersData)
+//     displayPlayers(playersData); 
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// function displayPlayers(players) {
+//   const contentDiv = document.getElementById('content');
+
+//   players.forEach(player => {
+//     let divCard = document.createElement("div");
+//     divCard.classList.add("card");
+//     divCard.setAttribute("id", `card${player.id}`);
+
+//     divCard.innerHTML = `
+//       <h3 class="player-name">${player.firstName}</h3>
+//       <img src="${player.avatarUrl}" class="player-photo" alt="${player.name}">
+//       <img src="${player.club}" class="club-logo" alt="Club">
+//       <img src="${player.flag}" class="flag" alt="Flag">
+//       <p class="position">${player.position}</p>
+//       <h5 class="rating">Rating: ${player.rating}</h5>
+//       <ul class="stats">
+//         <li>Pace: ${player.pace}</li>
+//         <li>Shooting: ${player.shooting}</li>
+//         <li>Passing: ${player.passing}</li>
+//         <li>Dribbling: ${player.dribbling}</li>
+//         <li>Defending: ${player.defending}</li>
+//         <li>Physical: ${player.physical}</li>
+//       </ul>
+//     `;
+
+//     contentDiv.appendChild(divCard);
+//   });
+// }
+
 
